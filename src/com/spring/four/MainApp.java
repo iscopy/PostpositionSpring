3d8566b7fd96a738b09123e794e43cb6e4de81be
caikp <caikp@zhenezhen.com>
2018-05-09 16:06:17 +0800

    示例

--- src/com/spring/four/MainApp.java
+++ src/com/spring/four/MainApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 在初始化前和初始化后实现
 
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		HelloWorld obj = (HelloWorld) context.getBean("postposition");
